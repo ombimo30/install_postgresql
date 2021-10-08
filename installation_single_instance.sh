0f29a66a0e2e4bcf4ae3424ecda050f35393626b
ombimo30 <50820557+ombimo30@users.noreply.github.com>
2021-10-08 21:35:24 +0700

    Update installation_single_instance.sh

--- installation_single_instance.sh
+++ installation_single_instance.sh
@@ -16,6 +16,8 @@ export ADMUSER=''
 export ADMPASSWORD=''
 export DBNAME=''
 export PORT=''
+export WORKING_DIR='/home/postgres'
+export LOG_FILE_FORMAT='postgresql-$VERSION.log'
 
 # Create the file repository configuration
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
